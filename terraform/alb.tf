3171911f6b50ea66027e44c623a94687f494b0b7
Tang <jason.tang@kpmg.co.uk>
2021-12-26 16:12:55 +0000

    alb

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,4 +1,4 @@
-resource "aws_lb" "test" {
+resource "aws_lb" "ecs" {
   name               = "${var.project_name}-alb-ecs"
   internal           = false
   load_balancer_type = "application"
@@ -7,9 +7,44 @@ resource "aws_lb" "test" {
 
   enable_deletion_protection = true
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "${var.project_name}-lb"
-    enabled = true
+#   access_logs {
+#     bucket  = aws_s3_bucket.b.bucket
+#     prefix  = "${var.project_name}-lb"
+#     enabled = true
+#   }
+}
+
+resource "aws_lb_target_group" "ecs" {
+  name     = "${var.project_name}-ecs"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    interval            = 300
+    path                = "/"
+    timeout             = 60
+    matcher             = "200"
+    healthy_threshold   = 5
+    unhealthy_threshold = 5
+  }
+
+  tags = {
+    Name = "dev-to"
+    Project = "dev-to"
+    Billing = "dev-to"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.ecs.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs.arn
   }
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,8 +1,23 @@
-resource "aws_s3_bucket" "lb_logs" {
-  bucket = "${var.project_name}_logs"
+# resource "aws_s3_bucket" "lb_logs" {
+#   bucket = "${var.project_name}-logs"
+#   acl    = "log-delivery-write"
+
+#   versioning {
+#     enabled = true
+#   }
+# }
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "${var.project_name}-log-bucket"
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "${var.project_name}-test-bucket"
   acl    = "private"
 
-  versioning {
-    enabled = true
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
   }
 }
