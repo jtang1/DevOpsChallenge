fbf2afd25294db4ff362864e21daa9afc8651dcf
Tang <jason.tang@kpmg.co.uk>
2021-12-29 14:36:08 +0000

    test s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -15,3 +15,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.lb_logs.id
   policy = data.local_file.s3_access_log_policy.content
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-devops-challenge"
+  acl    = "private"
+}
