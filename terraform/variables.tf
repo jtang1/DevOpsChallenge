43a312b8c909edd1287c5a828e0c7cefe1cd080c
Tang <jason.tang@kpmg.co.uk>
2021-12-30 11:18:14 +0000

    adding varaibles into json

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,14 @@
+variable "account_id" {
+  default = "836668077738"
+}
+
 variable "project_name" {
   default = "devopschallenge"
+}
+
+variable "elb_account_id" {
+  default = 156460612806
+  description = "A region specific Elastic Load Balancing account ID. The default is set for eu-west-1."
 }
 
 variable "region" {
